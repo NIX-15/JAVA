96e8db2f7e4093054af246c509b437edbfa64a0d
Nikita <nix15@bk.ru>
2025-01-11 02:06:23 +0300

    Data types_update

--- lection1/lection1.java
+++ lection1/lection1.java
@@ -23,11 +23,25 @@ public static void main(String[] args) {
     System.out.println(flag3);
     String msg = "This was the basics of data types";
     System.out.println(msg);
-    msg = "Go"; // Ссылка на объект переопредилилась на другой участок памяти, но предыдущее значение осталось где-то без ссылки.
+    msg = "Go"; // Ссылка на объект переопредилилась на другой участок памяти, но предыдущее значение осталось где-то без ссылки. 
+    // Надо использовать stringbuilder, stringbuffer для изменения строки правильно
     System.out.println(msg);
     String str1 = "Hello";
-    String str2 = new String("Hello");
-    System.out.println(str1==str2);
-    System.out.println(str1.equals(str2));
+    String str2 = new String("Hello"); // есть пул строк(оптимизация) а есть прямое создание через объект класса.
+    System.out.println(str1==str2); // разные объекты в памяти, но если обе строки через пул, то адрес будет один (true)
+    System.out.println(str1.equals(str2)); // содержимое одинаково
+    var a = 99;
+    System.out.println(a);
+    var b = 'r';
+    System.out.println(b);
+    System.out.println(getType(a));
+    System.out.println(getType(b));
+  
+    
+    
 }
+static String getType(Object obj) {
+    return obj.getClass().getSimpleName(); //порой надо прописывать классы для применения методов
+    //в частности, для определения типа данных примитива придется вызывать именно метод, написанный во вне.
+} 
 }
