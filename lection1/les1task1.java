9c378e9fe94a1e6caa94a8a463786f006c1a7a98
Nikita <nix15@bk.ru>
2025-01-19 16:38:20 +0300

    lec1_task2_in_progress1!

--- lection1/les1task1.java
+++ lection1/les1task1.java
@@ -14,7 +14,6 @@ public class les1task1 {
 
     static int[] filledArr(Scanner iScan) {
         Random rand = new Random();
-        //Scanner iScanner = new Scanner(System.in);
         System.out.printf("Введите степень многочлена : ");
         int k = iScan.nextInt();
 
@@ -23,7 +22,7 @@ public class les1task1 {
             System.out.printf("Введите степень многочлена : ");
             k = iScan.nextInt();
         }
-        //iScanner.close();
+        // iScanner.close();
         int[] arr = new int[k + 1];
         for (int i = arr.length - 1; i >= 0; i--) {
             arr[i] = rand.nextInt(201) - 100;
@@ -76,7 +75,7 @@ public class les1task1 {
 
         }
         String polynome = sb.toString();
-        //System.out.println(polynome);
+        // System.out.println(polynome);
         try (
 
                 FileWriter fw = new FileWriter(fileName, false)) {
@@ -87,55 +86,3 @@ public class les1task1 {
         }
     }
 }
-
-/*
- * for (int i = arr.length - 1; i >= 0; i--) {
- * arr[i] = rand.nextInt(201) - 100;
- * 
- * if (i == arr.length - 1) {
- * if (arr[arr.length - 1] == 0) {
- * arr[arr.length - 1] = 1;// что бы не нарушалась степень многочлена k
- * }
- * if (Math.abs(arr[arr.length - 1]) == 1) {
- * 
- * System.out.print(arr[arr.length - 1] == 1 ? "x^" + (arr.length - 1) : "-x^" +
- * (arr.length - 1)); // что
- * // бы
- * // не
- * // показывал
- * // 1x...
- * } else {
- * System.out.print(arr[arr.length - 1] + "x^" + (arr.length - 1));
- * }
- * }
- * else if (i < arr.length - 1 && i > 1) {
- * if (arr[i] != 0) {
- * if (Math.abs(arr[i]) == 1) {
- * System.out.print(arr[i] == 1 ? "+x^" + i : "-x^" + i);
- * } else {
- * System.out.print(arr[i] > 0 ? "+" + arr[i] + "x^" + i : arr[i] + "x^" + i);
- * }
- * 
- * }
- * }
- * else if (i==1){
- * //arr[i]=0;
- * if (arr[i] != 0) {
- * if (Math.abs(arr[i]) == 1) {
- * System.out.print(arr[i] == 1 ? "+x" : "-x");
- * } else {
- * System.out.print(arr[i] > 0 ? "+" + arr[i] + "x" : arr[i] + "x");
- * }
- * 
- * }
- * }
- * else {
- * //arr[0]=0;
- * if (arr[0] != 0) {
- * System.out.println(arr[0] > 0 ? "+" + arr[0] : arr[0]);
- * }
- * 
- * }
- * 
- * }
- */
--- lection1/les1task2.java
+++ lection1/les1task2.java
@@ -2,38 +2,73 @@ package JAVA.lection1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import JAVA.lection1.les1task1;
 
 public class les1task2 {
     public static void main(String[] args) {
-
+        getArr(1);
+        getArr(2);
     }
 
     static int[] getArr(int q) {
-        int[] arr = new int[0];
+        ArrayList<Integer> list = new ArrayList<>();
         String fileName = "file" + q + ".txt";
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        StringBuilder coefficient = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            // for (int i = 0; i < line.length(); i++) {
-            /*
-             * char c = line.charAt(i);
-             * 
-             * // Если символ - цифра или знак +/-, добавляем его к текущему коэффициенту
-             * if (Character.isDigit(c) || c == '-') {
-             * coefficient.append(c);
-             * }
-             * 
-             * // Когда встречаем 'x', добавляем текущий коэффициент в список и очищаем
-             * текущее значение
-             * if (c == 'x') {
-             * line.add(coefficient.toString());
-             * coefficient.setLength(0);
-             * }
-             */
-            return arr;
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            sb.append(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace(); // Обработка исключений
         }
-    
+        StringBuilder tempSB = new StringBuilder();
+        for (int i = 0; i < sb.length(); i++) {
+            char currentChar = sb.charAt(i); // текущий символ
+
+            if (i == 0) {
+                while (currentChar != 'x') {
+                    tempSB.append(currentChar);
+                    i++;
+                }
+                try {
+                    if (tempSB.length() == 1 && tempSB.charAt(0) == '-') {
+                        list.add(-1);
+                    } else if (tempSB.length() == 0) {
+                        list.add(1);
+                    } else {
+                        list.add(Integer.parseInt(tempSB.toString()));
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Ошибка преобразования: " + e.getMessage());
+                }
+                tempSB.setLength(0);
+            } else if (sb.charAt(i) == 'x') {
+                while (currentChar != '+' || currentChar != '-') {
+                    tempSB.append(currentChar - 1);
+                    if (currentChar - 2 == '-') {
+                        tempSB.append(currentChar - 2);
+                    }
+                    tempSB.reverse();
+                    try {
+                        if (tempSB.length() == 1 && tempSB.charAt(0) == '-') {
+                            list.add(-1);
+                        } else if (tempSB.length() == 0) {
+                            list.add(1);
+                        } else {
+                            list.add(Integer.parseInt(tempSB.toString()));
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Ошибка преобразования: " + e.getMessage());
+                    }
+                }
+                tempSB.setLength(0);
+            }
+            else if(i==sb.length()-1){
+
+            }
+        }
+        return list;
+
+    }
+
 }
